471d349bc0b52fdc40cffdeb28c76b2d91d3958b
Lin Yinfeng <lin.yinfeng@outlook.com>
2019-05-15 11:38:49 +0800

    Keep both system and user versin of fonts

--- bin/generate-manifests.ps1
+++ bin/generate-manifests.ps1
@@ -14,6 +14,42 @@
     },
     "installer": {
         "script": [
+            "if(!(is_admin)) { error \"Admin rights are required, please run 'sudo scoop install `$app'\"; exit 1 }",
+            "Get-ChildItem `$dir -filter '*Windows Compatible.*' | ForEach-Object {",
+            "    New-ItemProperty -Path 'HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts' -Name `$_.Name.Replace(`$_.Extension, ' (TrueType)') -Value `$_.Name -Force | Out-Null",
+            "    Copy-Item \"`$dir\\`$_\" -destination \"`$env:windir\\Fonts\"",
+            "}"
+        ]
+    },
+    "uninstaller": {
+        "script": [
+            "if(!(is_admin)) { error \"Admin rights are required, please run 'sudo scoop uninstall `$app'\"; exit 1 }",
+            "Get-ChildItem `$dir -filter '*Windows Compatible.*' | ForEach-Object {",
+            "    Remove-ItemProperty -Path 'HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts' -Name `$_.Name.Replace(`$_.Extension, ' (TrueType)') -Force -ErrorAction SilentlyContinue",
+            "    Remove-Item \"`$env:windir\\Fonts\\`$(`$_.Name)\" -Force -ErrorAction SilentlyContinue",
+            "}",
+            "Write-Host \"The '`$(`$app.Replace('-NF', ''))' Font family has been uninstalled and will not be present after restarting your computer.\" -Foreground Magenta"
+        ]
+    }
+}
+"@
+
+$userTemplateString = @"
+{
+    "version": "0.0",
+    "license": "MIT",
+    "homepage": "https://github.com/ryanoasis/nerd-fonts",
+    "url": " ",
+    "hash": " ",
+    "checkver": {
+        "url": "https://github.com/ryanoasis/nerd-fonts/releases",
+        "re": "/ryanoasis/nerd-fonts/releases/download/v(?<version>[\\d\\.]+)/%name.zip"
+    },
+    "autoupdate": {
+        "url": "https://github.com/ryanoasis/nerd-fonts/releases/download/v`$version/%name.zip"
+    },
+    "installer": {
+        "script": [
             "New-Item -Path 'HKCU:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts\\Scoop.%name' -Force | Out-Null",
             "Get-ChildItem `$dir -filter '*Windows Compatible.*' | ForEach-Object {",
             "    New-ItemProperty -Path 'HKCU:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts\\Scoop.%name' -Name `$_.Name -Value `$_.FullName -Force | Out-Null",
@@ -32,14 +68,20 @@
 $fontNames = @(
     "3270",
     "AnonymousPro",
+    "Arimo",
     "AurulentSansMono",
+    "BigBlueTerminal",
     "BitstreamVeraSansMono",
+    "Bold",
+    "BoldItalic",
     "CodeNewRoman",
+    "Cousine",
     "DejaVuSansMono",
     "DroidSansMono",
     "FantasqueSansMono",
     "FiraCode",
     "FiraMono",
+    "Go-Mono",
     "Gohu",
     "Hack",
     "Hasklig",
@@ -47,7 +89,9 @@ $fontNames = @(
     "Hermit",
     "Inconsolata",
     "InconsolataGo",
+    "InconsolataLGC",
     "Iosevka",
+    "Italic",
     "Lekton",
     "LiberationMono",
     "Meslo",
@@ -55,20 +99,26 @@ $fontNames = @(
     "Monoid",
     "Mononoki",
     "MPlus",
+    "Noto",
+    "OpenDyslexic",
+    "Overpass",
     "ProFont",
     "ProggyClean",
+    "Regular",
     "RobotoMono",
     "ShareTechMono",
     "SourceCodePro",
     "SpaceMono",
     "Terminus",
+    "Tinos",
     "Ubuntu",
     "UbuntuMono"
 )
 
 # Generate manifests
 $fontNames | ForEach-Object {
-    $templateString -replace "%name", $_ | Out-File -FilePath "$PSScriptRoot\..\$_-NF-User.json" -Encoding utf8
+    $templateString -replace "%name", $_ | Out-File -FilePath "$PSScriptRoot\..\bucket\$_-NF.json" -Encoding utf8
+    $userTemplateString -replace "%name", $_ | Out-File -FilePath "$PSScriptRoot\..\bucket\$_-NF-User.json" -Encoding utf8
 }
 
 # Use scoop's checkver script to autoupdate the manifests
