78363f1026c03d2d8f929e48cbc5edd8b7a21d37
Lin Yinfeng <lin.yinfeng@outlook.com>
2019-05-16 05:07:28 +0800

    Add update script with sleep to pass rate limit of GitHub. Update all manifest files.

---
+++ bin/update-slow.ps1
@@ -0,0 +1,7 @@
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (Split-Path (Split-Path (scoop which scoop))) }
+$checkver = "$env:SCOOP_HOME\bin\checkver.ps1"
+$dir = "$PSScriptRoot\..\bucket" # checks the parent dir
+Get-ChildItem $dir | Foreach-Object {
+    Invoke-Expression -Command ("$checkver -Dir $dir {0} -u" -f $_.BaseName)
+    Start-Sleep -Seconds 5
+}
